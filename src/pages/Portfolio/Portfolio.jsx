668a095c26dfe5e732c7a8971ac6c1a335e33cc1
thadeus <thadeusnyariki94@gmail.com>
2023-04-27 01:42:14 +0300

    Complted Portfolio

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -3,7 +3,44 @@ import './Portfolio.scss'
 
 const Portfolio = () => {
   return (
-    <div className='portfolio'>Portfolio</div>
+    <div className='portfolio'>
+      <div className="top">
+        <span style={{color: '#f40840', textTransform: 'uppercase', fontWeight: 500}}>award winning work</span>
+        <h2>recent works</h2>
+      </div>
+      <div className="works">
+        <div className="item">
+          <div className="item-browser">
+            <div className="item-circle"></div>
+            <div className="item-circle"></div>
+            <div className="item-circle"></div>
+          </div>
+          <a href="http://" target="_blank" rel="noreferrer">
+            <img src={require('../../assets/carguruz.png')} alt="" />
+          </a>
+        </div>
+        <div className="item">
+          <div className="item-browser">
+            <div className="item-circle"></div>
+            <div className="item-circle"></div>
+            <div className="item-circle"></div>
+          </div>
+          <a href="http://" target="_blank" rel="noreferrer">
+            <img src={require('../../assets/fiverr.png')} alt="" />
+          </a>
+        </div>
+        <div className="item">
+          <div className="item-browser">
+            <div className="item-circle"></div>
+            <div className="item-circle"></div>
+            <div className="item-circle"></div>
+          </div>
+          <a href="http://" target="_blank" rel="noreferrer">
+            <img src={require('../../assets/ecommerce.png')} alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
 
