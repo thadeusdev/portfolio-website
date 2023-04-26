133d5f6e78a1e5e24fe6fcae58560bb96658ba1d
thadeus <thadeusnyariki94@gmail.com>
2023-04-26 18:39:06 +0300

    Completed home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
 import './Home.scss'
+import { Link } from 'react-router-dom';
+import { 
+  FiTwitter, 
+  FiGithub, 
+  FiLinkedin 
+} from "react-icons/fi";
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <div className="left">
+        <div className="intro">
+          <h3>Hello, <span>I'm</span></h3>
+          <h2>Thadeus Nyariki</h2>
+          <span>Full Stack Software Developer</span>
+        </div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, laborum doloremque mollitia sunt praesentium itaque possimus quisquam eveniet dolorem, autem hic nemo, quasi consequuntur tempore ea vero incidunt accusamus doloribus omnis sint placeat! Nam tempora asperiores magni quos, rerum harum!
+        </p>
+        <div className="social">
+          <h3>find me on</h3>
+          <div className="social-items">
+            <Link className='social-item'>
+              <FiGithub/>
+            </Link>
+            <Link className='social-item'>
+              <FiLinkedin/>
+            </Link>
+            <Link className='social-item'>
+              <FiTwitter/>
+            </Link>
+          </div>
+        </div>
+        <div className="featured">
+          <div className="featured-item">
+            <h3>2+</h3>
+            <span>year of experience</span>
+          </div>
+          <div className="featured-item">
+            <h3>700+</h3>
+            <span>global working client</span>
+          </div>
+          <div className="featured-item">
+            <h3>30+</h3>
+            <span>awards win</span>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <div className="frame"></div>
+        <img src={require('../../assets/hero.png')} alt="" />
+      </div>
+    </div>
   )
 }
 
