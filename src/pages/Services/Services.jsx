b90b8f6503a1407948b1e5a5bdbd141aec9f0ae7
thadeus <thadeusnyariki94@gmail.com>
2023-04-27 00:39:02 +0300

    Complted Services

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -12,21 +12,21 @@ const Services = () => {
       <span style={{color: '#f40840', textTransform: 'uppercase', fontWeight: 500}}>my services</span>
       <h2>what i do</h2>
       <div className="items">
-        <div className="item">
+        <div className="item">          
           <FaDesktop className='icon'/>
           <h3>Quality Websites</h3>
           <span>
             Quality is the first thing I think about when working on a project. Almost 80% of visitors are on mobile, so responsiveness and mobile-first are my goals while developing a website.
           </span>
         </div>
-        <div className="item">
+        <div className="item">          
           <FaLaptop className='icon'/>
           <h3>Design aimed at users</h3>
           <span>
             Designing engaging and meaningful user experiences is a competitive advantage to differentiate yourself from competitors, satisfy your customers and, consequently, achieve your business goals.
           </span>
         </div>
-        <div className="item">
+        <div className="item">          
           <FaRegLightbulb className='icon'/>
           <h3>A Creative Mind</h3>
           <span>
