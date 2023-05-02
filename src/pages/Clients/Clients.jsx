e13e1805c3ebfb48dc3e302313bd88449809ac4b
thadeus <thadeusnyariki94@gmail.com>
2023-05-02 23:09:36 +0300

    Completed clients

--- src/pages/Clients/Clients.jsx
+++ src/pages/Clients/Clients.jsx
@@ -3,7 +3,38 @@ import './Clients.scss'
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <div className='clients'>
+      <div className="top">
+        <span style={{color: '#f40840', textTransform: 'uppercase', fontWeight: 500}}>best customers</span>
+        <h2>selected clients</h2>
+      </div>
+      <div className="bottom">
+        <div className="bottom-item">
+          <img src={require('../../assets/Amazon.png')} alt="" />
+        </div>
+        <div className="bottom-item">
+          <img src={require('../../assets/paix.png')} alt="" />
+        </div>
+        <div className="bottom-item">
+          <img src={require('../../assets/microsoft.png')} alt="" />
+        </div>
+        <div className="bottom-item">
+          <img src={require('../../assets/google.png')} alt="" />
+        </div>
+        <div className="bottom-item">
+          <img src={require('../../assets/SpaceX.png')} alt="" />
+        </div>
+        <div className="bottom-item">
+          <img src={require('../../assets/Diageo.png')} alt="" />
+        </div>
+        <div className="bottom-item">
+          <img src={require('../../assets/facebook.png')} alt="" />
+        </div>
+        <div className="bottom-item">
+          <img src={require('../../assets/MAN.png')} alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
